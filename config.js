efadc3868431bdc426561676505dd5230019a17a
abir9865 <abirahmed843268@gmail.com>
2025-01-04 09:45:15 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "MTMyNDc1MDQ4NzQzNzcwNTIxNg.GJ97vj.jgPKxrDVViuKXSY_RbIhFDdeJSJJQ4mXepCf9E",
+  TOKEN: "",
   ownerID: ["860761801855205396", "1293989203482247290"],
   botInvite: "https://discord.com/oauth2/authorize?client_id=1324750317526454284&permissions=8&integration_type=0&scope=bot",
   supportServer: "https://discord.gg/tbMNNnwDRg",
