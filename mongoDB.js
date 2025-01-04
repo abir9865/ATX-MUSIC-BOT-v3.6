fe21d50132183f167360a120b319a8a639e0963b
abir9865 <abirahmed843268@gmail.com>
2025-01-04 09:07:28 +0600

    Update mongoDB.js

--- mongoDB.js
+++ mongoDB.js
@@ -44,8 +44,8 @@ module.exports = {
   ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
 
    
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   # MADE BY ATX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/tbMNNnwDRg ]
+   ## YT : https://discord.gg/tbMNNnwDRg
 */
