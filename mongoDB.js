8357e2e106e6a23de05f28b65c96891cda9a2f94
abir9865 <abirahmed843268@gmail.com>
2025-01-04 09:07:06 +0600

    Update mongoDB.js

--- mongoDB.js
+++ mongoDB.js
@@ -8,10 +8,10 @@
   ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
 
    
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   # MADE BY ATX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/tbMNNnwDRg ]
+   ## YT : https://discord.gg/tbMNNnwDRg
 */
 const { Schema, model } = require("mongoose");
 
